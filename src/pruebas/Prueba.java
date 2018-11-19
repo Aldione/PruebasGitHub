2d9a329df35cea6646171bbde5877a99143f4674
Aldione <tony.luque15@gmail.com>
2018-11-19 09:39:27 +0100

    Empiezo codigo

---
+++ src/pruebas/Prueba.java
@@ -0,0 +1,15 @@
+package pruebas;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		int i = 0;
+		boolean exit=false;
+		
+		if (i>=1) {
+			System.out.println(exit);
+		}
+
+	}
+
+}
